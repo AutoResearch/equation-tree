3bf7296445948bd04f49ba1274d55796d8a85044
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-08-12 07:57:49 -0400

    commit: initial commit

---
+++ src/equation_tree/__init__.py
@@ -0,0 +1,10 @@
+# from equation_tree.sample.tree import sample_tree
+#
+# np.random.seed(42)
+# max_depth = 5
+# feature_priors = {"x_1": 0.5, "c_1": 0.5}
+# function_priors = {"sin": 0.5, "cos": 0.5}
+# operator_priors = {"+": 0.5, "-": 0.5}
+# node_root = sample_tree(max_depth, feature_priors, function_priors, operator_priors)
+# equation_tree = EquationTree(node_root)
+# equation_tree.structure
