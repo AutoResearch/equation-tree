43e53010f754e6110356ccb4ea94f9e51cf5005d
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-08-13 21:43:15 -0400

    feat: add conditional priors

---
+++ src/equation_tree/analysis.py
@@ -0,0 +1,9 @@
+from typing import List
+from equation_tree.tree import EquationTree
+
+
+# TODO: implement this
+def get_frequencies(trees: List[EquationTree]):
+    for t in trees:
+        print(t.info)
+    return {}
